2e6b8f94739fa7718dd09952f323e3a372c7b3f9
Manixhor <manigururam@hotmail.com>
2026-08-22 01:43:41 +0530

    fixed currency

--- .freebuff/start.sh
+++ .freebuff/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd /Users/mani/Documents/production1
+export PATH="/opt/homebrew/bin:/usr/local/bin:$PATH"
 export DB_ENGINE=sqlite
-exec .venv/bin/python manage.py runserver 0.0.0.0:8080
+cd /Users/mani/Documents/production1
+exec /Users/mani/Documents/production1/.venv/bin/python manage.py runserver 0.0.0.0:8000
